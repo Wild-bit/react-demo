d0a3056c5a4c816b044bc7cae0224a4744d20b30
Wild-bit <1397119446@qq.com>
2023-01-29 22:41:39 +0800

    feat: 2023第一次提交 \ jsx基础语法 条件渲染、样式绑定、jsx表达式、jsx注释

--- src/components/ListRender/ListRender.js
+++ src/components/ListRender/ListRender.js
@@ -1,9 +1,13 @@
 import "./ListRender.css"
 export default function ListRender() {
+  const jsx = "JSX表达式-用单括号包裹变量｛｝"
+  const flag = true
   const listSting = ["榴莲", "西瓜", "哈密瓜", "车厘子", "草莓"]
   return (
     <>
-      <div className="title">JSX-列表渲染</div>
+      <p className="title">JSX基础语法</p>
+      <p className="sub-title">{jsx}</p>
+      <div className="sub-title">JSX-列表渲染</div>
       <p className="desc">
         与vue使用指令v-for不同，react使用 Javascript转化列表，使用数组方法map
       </p>
@@ -12,6 +16,11 @@ export default function ListRender() {
           return <li key={index}>{item}</li>
         })}
       </ul>
+      <p className="sub-title">条件渲染</p>
+      <p className="desc">使用三元运算符或逻辑与运算符(&&)</p>
+      {/* 条件渲染字符串 */}
+      <p className="text">true渲染：{flag ? "react真有趣" : "vue真有趣"}</p>
+      <p className="text">false渲染：{!flag ? "react真有趣" : "vue真有趣"}</p>
     </>
   )
 }
