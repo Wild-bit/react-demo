b32c7385272108e0dfc23e7816a0b29dd48571f5
Wild-bit <1397119446@qq.com>
2023-02-01 23:10:18 +0800

    feat: 组件通信-子传父

--- src/components/ComponentsCommunication/ComponentsCommunication.js
+++ src/components/ComponentsCommunication/ComponentsCommunication.js
@@ -6,12 +6,20 @@ class ComponentsCommunication extends Component {
     son1Message:
       "类组件的父传子通信，由于类组件继承了Component的props属性，所以在内部可以直接通过this.props访问",
     son2Message: "函数组件的父传子可通过参数props来访问",
+    receiveSonqMessage: "",
+  }
+  changeMsg = (newMessage) => {
+    console.log(newMessage)
+    this.setState({
+      receiveSonqMessage: newMessage,
+    })
   }
   render() {
     return (
       <div className="father">
         <p>父组件</p>
-        <Son1 message={this.state.son1Message} />
+        <p>{this.state.receiveSonqMessage}</p>
+        <Son1 message={this.state.son1Message} changeMsg={this.changeMsg} />
         <Son2 message={this.state.son2Message} />
       </div>
     )
@@ -19,12 +27,19 @@ class ComponentsCommunication extends Component {
 }
 
 class Son1 extends Component {
+  handleClick = () => {
+    // 调用父组件传递过来的回调函数 并注入参数
+    this.props.changeMsg(
+      "我是Son1，通过调用父组件传递过来的回调函数 并注入所需的参数来实现子传父的通信"
+    )
+  }
   render() {
     const { message } = this.props
     return (
       <div className="son1">
         <p>类子组件</p>
         <p>{message}</p>
+        <button onClick={this.handleClick}>子传父</button>
       </div>
     )
   }
